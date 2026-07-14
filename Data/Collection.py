8e670fae09d4ae0a755ca3c530293790a87e07e7
kiruba192k2 <jonashtyler@gmail.com>
2026-07-14 17:59:11 +0530

    vc nxvx

---
+++ Data/Collection.py
@@ -0,0 +1,14 @@
+from collections import *
+a ="kiruba"
+print(Counter(a))
+
+
+data = defaultdict(int)
+a="dgtsdhnfnfsgfdgrsdshtjsrg"
+
+for i in a:
+    data[i] += 1
+
+print(data)
+
+print(Counter(a))
--- Format.py
+++ Format.py
@@ -1,4 +1,4 @@
-string1 = 'Student : {name} Marks : {marks}'
+'''string1 = 'Student : {name} Marks : {marks}'
 print(string1.format(name="Aarav", marks=95))
 print('Student : {1} Marks : {0}'.format('88', 'Priya'))
 print(f'{25:b}')
@@ -10,4 +10,15 @@ print(f'{45:5}')
 print(f'{45:3}')
 print(f'{45:04}')
 print(f'{987654:,}')
-print(f'{0.675:.2%}')
+print(f'{0.675:.2%}')'''
+
+l=[1,4,5,6,2]
+print(sorted(l))
+print(max(l))
+print(min(l))
+print(list(filter(lambda x:x%2,l)))
+print(list(map(lambda x:x**2,l)))
+l2=[[1,2,3],[4,6,7],[5,8,9]]
+print(list(zip(*l2)))
+print(any(i%2==0 for i in [7,3,5]))
+ 
